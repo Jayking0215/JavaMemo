1f2aaea27b919100373a9670804e4ca2e153ecbe
kjm6890 <zaesoon2@gmail.com>
2023-04-12 01:07:33 +0900

    DBUtil 추가

--- JavaGUI/src/javaMemo/MemoDAO.java
+++ JavaGUI/src/javaMemo/MemoDAO.java
@@ -1,5 +1,21 @@
 package javaMemo;
 
-public class MemoDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class MemoDAO {
+	private Connection con;
+	private PreparedStatement ps;
+	private ResultSet rs;
+	
+	
+	
+	public void insertMemo(MemoVO memo) {
+		
+		
+	}
 }
