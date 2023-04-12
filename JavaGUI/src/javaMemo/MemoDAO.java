8854f4812528170973b38460b8fa03cddd327d8f
JJJ <zaesoon2@naver.com>
2023-04-13 08:50:15 +0900

    MemoDAO 완성

--- JavaGUI/src/javaMemo/MemoDAO.java
+++ JavaGUI/src/javaMemo/MemoDAO.java
@@ -1,11 +1,10 @@
 package javaMemo;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.*;
+import common.util.*;
+
+import common.util.*;
 
 public class MemoDAO {
 	private Connection con;
@@ -16,8 +15,134 @@ public class MemoDAO {
 	 * 한줄 메모장에 insert문을 수행하는 메서드
 	 * */ //MemoVO 자체가 1개의 행
 	
-	public void insertMemo(MemoVO memo) {
-		
+	public int insertMemo(MemoVO memo) throws SQLException {
+		try {
+			con=DBUtil.getCon();
+			StringBuilder buf=new StringBuilder("INSERT INTO MEMO(no,name,msg,wdate)")
+							.append(" VALUES(MEMO_SEQ.NEXTVAL,?,?,SYSDATE");
+			String sql=buf.toString();
+			
+			ps=con.prepareStatement(sql);
+			ps.setString(1, memo.getName());
+			ps.setString(2, memo.getMsg());
+			
+			int n=ps.executeUpdate();
+			return n;
+		}finally {
+			close();
+		}
+	}//----
+	
+	/**전체 메모글을 가져오는 메서드
+	 * */ //memoVO자체가 행 1줄
+	public List<MemoVO> listMemo() throws SQLException{
+		try {
+			con=DBUtil.getCon();
+			StringBuilder buf=new StringBuilder("SELECT RPAD(no,10,' ') no, RPAD(name,16,' ') name,")
+							.append(" RPAD(msg,100,' '), wdate FROM MEMO ORDER BY wdate DESC");
+			String sql=buf.toString();
+			
+			ps=con.prepareStatement(sql);
+			rs=ps.executeQuery();
+			
+			List<MemoVO> arr=makeList(rs);
+			return arr;
+		}finally {
+			close();
+		}
+	}//----
+	
+	private List<MemoVO> makeList(ResultSet rs) throws SQLException {
+		List<MemoVO> arr=new ArrayList<>();
+		while(rs.next()) {
+			int no=rs.getInt("no");
+			String name=rs.getString("name");
+			String msg=rs.getString("msg");
+			java.sql.Date wdate=rs.getDate("wdate");
+			//오버로드
+			MemoVO memo=new MemoVO(no,name,msg,wdate);
+			arr.add(memo);
+		}
+		return arr;
+	}//----
+	
+	/** 글번호로(PK)로 메모글을 가져오는 메서드 
+	 * */
+	public MemoVO selectMemo(int no) throws SQLException{
+		try {
+			con=DBUtil.getCon();
+			String sql="SELECT no,name,msg,wdate FROM MEMO WHERE no=?";
+			ps=con.prepareStatement(sql);
+			ps.setInt(1, no);
+			rs=ps.executeQuery();
+			List<MemoVO> arr=makeList(rs);
+			if(arr!=null && arr.size()==1) {
+				return arr.get(0);
+			}
+			return null;//해당글이 없을 경우
+		}finally {
+			close();
+		}
+	}//----
+	
+	/** keyword로 메모 글 내용을 검색하는 메서드
+	 * */
+	public List<MemoVO> findMemo(String keyword) throws SQLException{
+		try {
+			con=DBUtil.getCon();
+			StringBuilder buf=new StringBuilder("SELECT RPAD(no,10,' ')no,")
+						.append(" RPAD(name,16,' ')name, RPAD(msg,100,' ')msg, wdate FROM MEMO")
+						.append(" WHERE msg LIKE ?");
+			String sql=buf.toString();
+			ps=con.prepareStatement(sql);
+			ps.setString(1,  "%"+keyword+"%");
+			rs=ps.executeQuery();
+			List<MemoVO> arr=makeList(rs);
+			return arr;
+		}finally {
+			close();
+		}
+	}//----
+	
+	/**메모 글 내용,작성자를 수정하는 메서드
+	 * */
+	public int updateMemo(MemoVO vo) throws SQLException{
+		try {
+			con=DBUtil.getCon();
+			String sql="UPDATE MEMO SET name=?, msg=? WHERE no=?";
+			ps=con.prepareStatement(sql);
+			ps.setString(1, vo.getName());
+			ps.setString(2, vo.getMsg());
+			ps.setInt(3, vo.getNo());
+			
+			return ps.executeUpdate();
+		}finally {
+			close();
+		}
+	}//----
+	
+	/**글번호로 메모글을 삭제하는 메서드
+	 * */
+	public int deleteMemo(int no) throws SQLException{
+		try {
+			con=DBUtil.getCon();
+			String sql="DELETE FROM MEMO WHERE no=?";
+			ps=con.prepareStatement(sql);
+			ps.setInt(1, no);
+			return ps.executeUpdate();
+		}finally {
+			close();
+		}
+	}//----
+
+	private void close() {
+		try {
+			if(rs!=null)rs.close();
+			if(ps!=null)ps.close();
+			if(con!=null)con.close();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
 		
 	}
 }
--- JavaGUI/src/javaMemo/MemoHandler.java
+++ JavaGUI/src/javaMemo/MemoHandler.java
@@ -1,6 +1,63 @@
 package javaMemo;
 
+import java.awt.event.*;
+import java.sql.*;
+import java.util.*;
 
-public class MemoHandler {
+public class MemoHandler implements ActionListener {
+	MemoAppView app;
+	MemoDAO dao=new MemoDAO();
+	
+	public MemoHandler(MemoAppView mav) {
+		this.app=mav;
+	}
+	
+	@Override
+	public void actionPerFormed(ActionEvent e) {
+		Object obj=e.getSource();
+		if(obj==app.btAdd) {//글 추가
+			addMemo();
+			app.clearInput();//전체 글목록 비우기
+			listMemo();//전체목록 출력 메서드
+		}else if(obj==app.btList) {//글 목록보기
+			listMemo();
+		}else if(obj==app.btDel) {//글 삭제
+			deleteMemo();
+			listMemo();
+		}else if(obj==app.btEdit) {//글 수정
+			editMemo();
+		}else if(obj==app.btEditEnd) {//글 수정처리
+			editMemoEnd();
+			app.clearInput();
+			listMemo();
+		}else if(obj==app.btFind) {//글 내용 검색(키워드 검색)
+			findMemo();
+		}
+	}//-----
 
+	private void listMemo() {
+		
+	}
+
+	private void addMemo() {
+		
+	}
+
+	private void deleteMemo() {
+		
+	}
+
+	private void editMemoEnd() {
+		
+	}
+
+	private void editMemo() {
+		
+	}
+
+	private void findMemo() {
+		
+	}
+	
+	
 }
